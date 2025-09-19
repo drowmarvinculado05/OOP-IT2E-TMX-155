e6da301417b70f6eabfecbdf22a42cf154a39281
roniemanaongsong-afk <ronie.manaongsong@dorsu.edu.ph>
2025-09-19 23:00:08 +0800

    Update Person.java

--- Exer3_B. Manaongsong/Person.java
+++ Exer3_B. Manaongsong/Person.java
@@ -1,1 +1,27 @@
-
+public class Person {
+    protected String name;
+    protected String email;
+    protected String phone;
+    protected String address;
+    protected String dateOfBirth;
+    protected String gender;
+    
+    public Person(String name, String email, String phone, String address, 
+                 String dateOfBirth, String gender) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+    
+    public String displayInfo() {
+        return String.format("%s: %s, %s, %s, DOB: %s, Gender: %s", 
+                            name, email, phone, address, dateOfBirth, gender);
+    }
+    
+    public String contact() {
+        return "Contacting " + name + " at " + email + " or " + phone;
+    }
+}
